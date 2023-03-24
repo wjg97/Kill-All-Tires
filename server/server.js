0baaa477997223860a6806e7e13d5a9845042038
DylanCas <djcasabona12@gmail.com>
2023-03-23 20:01:05 -0600

    Implemented beginning of server setup. Able to add user, and get user info. TODO regarding ability to add vehicle related to user log in.

--- server/server.js
+++ server/server.js
@@ -17,14 +17,14 @@ const server = new ApolloServer({
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-/* Commented out to let server run in local host
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
-}); */
+}); 
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
