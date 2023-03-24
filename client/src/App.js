c725aa5b1dc6bcc66fa5e39de9ae78d3f8fc84f9
Persistent <agrlarbig@gmail.com>
2023-03-23 20:12:41 -0600

    Added some client side js

---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
