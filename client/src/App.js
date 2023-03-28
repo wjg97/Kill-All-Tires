137ce72967d43c2f4cb9dc56317e256998150b8e
Your Name <you@example.com>
2023-03-27 19:26:32 -0600

    Adding a contact form page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import About from "./components/about/About";
 import Main from "./components/Main";
+import Contact from "./components/contactForm/Contact";
 function App() {
   return (
     <div className="App">
-      <Main />
+      <Contact />
     </div>
   );
 }
