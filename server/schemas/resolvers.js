3d989f70f9963ec993018ee23ce6c1d30da0fdc3
DylanCas <djcasabona12@gmail.com>
2023-03-30 18:33:04 -0600

    Have gotten queries as settled as they may get. Cannot go too many layers deep. Likely need to make multiple queries to get data on page. But all data insertion/deletion and connection has been tested thoroughly.

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,6 +4,7 @@ const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
+    // **NOTE** Queries going deeper than 2 layers will require additional queries to get all data. Currently, queries such as User > Appointment > vehicle will return Null vehicle data. But a query for User, and Vehicle, and Appointment will return all data. So on so forth. All data has been tested and is working.
     // users will return all users and their vehicles
     users: async () => {
       return User.find().populate("vehicles");
@@ -14,7 +15,7 @@ const resolvers = {
     },
     // vehicles will return all vehicles
     vehicles: async () => {
-      return Vehicle.find().populate("user");
+      return Vehicle.find().populate("user"); 
     },
     // vehicle will return a single vehicle
     vehicle: async (parent, { vehicleId }) => {
