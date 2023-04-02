70ca7e2e2d9155bef505d2a8967e564505af54cf
Wyatt Gaulke <wyattgaulke@Wyatts-MacBook-Pro.local>
2023-04-01 22:10:39 -0600

    Queries.js and updated Seeds

--- server/server.js
+++ server/server.js
@@ -42,10 +42,5 @@ const startApolloServer = async (typeDefs, resolvers) => {
   })
   };
   
-
-  startApolloServer(typeDefs, resolvers);
-
-
 // Call the async function to start the server
-  startApolloServer(typeDefs, resolvers);
-
+startApolloServer(typeDefs, resolvers);
