16000fae453692f8c7ad74df6140dc873e84d0dc
Persistent <agrlarbig@gmail.com>
2023-04-03 22:52:11 -0600

    lots of testing and even more

---
+++ client/src/pages/NavTabs.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function NavTabs({ currentPage, handlePageChange }) {
+    return (
+      <ul className="navigation">
+        <li className="nav-item">
+          <a
+            href="#home"
+            onClick={() => handlePageChange('Home')}
+            // This is a conditional (ternary) operator that checks to see if the current page is "Home"
+            // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
+            className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
+          >
+            Home
+          </a>
+        </li>
+        </ul>
+    );
+  }
+  
+  export default NavTabs;
