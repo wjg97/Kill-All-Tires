cbca70e4a84bfa72350b55804d22e0d6c18724e5
DylanCas <djcasabona12@gmail.com>
2023-04-06 17:06:07 -0600

    Working towards resolution of signup.js. Do not believe id token is being generated, though cannot identify issue

--- client/src/pages/signupForm/signup.js
+++ client/src/pages/signupForm/signup.js
@@ -1,86 +1,74 @@
 import React, { useState} from "react";
 import { ADD_USER } from "../../utils/mutations"; // import the mutation
-import { ApolloProvider, useMutation } from "@apollo/client"; // import the useMutation hook
+import { useMutation } from "@apollo/client"; // import the useMutation hook
 import Auth from "../../utils/auth"; // import the Auth utility
-
-import logo from '../../assets/logo.svg'
+// import carGif from '../../assets/cargif.mp4';
+// import logo from '../../assets/logo.png';
 import './signup.css';
 
 
 const SignUp = () => {
   console.log("You are signing up for an account");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [username, setUsername] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
+  const [formState, setFormState] = useState({ username: "", email: "", password: "" });
+  const [addUser, { error, data }] = useMutation(ADD_USER); // use the useMutation hook to execute the ADD_USER mutation in the handleFormSubmit function
 
   const handleInputChange = (event) => {
-    const { target } = event;
-    const inputType = target.name;
-    const inputValue = target.value;
+    const { name, value } = event.target;
 
-    if (inputType === "email") {
-      setEmail(inputValue);
-    }
-    if (inputType === "password") {
-      setPassword(inputValue);
-    }
-    if (inputType === "username") {
-      setUsername(inputValue);
-    }
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
   };
 
-  const [addUser, { error }] = useMutation(ADD_USER); // use the useMutation hook to execute the ADD_USER mutation in the handleFormSubmit function
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
+    console.log(formState);
     
     try {
       const { data } = await addUser({
-        variables: { email, password, username },
+        variables: { ...formState },
       });
 
       Auth.login(data.addUser.token);
     } catch (e) {
       console.error(e);
-      setErrorMessage(e.message);
+      // setErrorMessage(e.message);
     }
-
-    setEmail("");
-    setPassword("");
-    setUsername("");
   };
 
 
   return (
     <div className="main">
+        {/* <video src={carGif} className="video" autoPlay loop muted/> */}
       <div className="content">
-        <img className="rotate" src={logo} alt="KAT logo" />
+        {/* <img className="rotate" src={logo} alt="KAT logo" /> */}
         <h1 className="animate">KILL ALL TIRES</h1>
         <p className="animate2">EST. 2023.</p>
       </div>
       <div className="signupContainer">
         <h1>Signup for an account</h1>
         <form onSubmit={handleFormSubmit}>
-        <p>Enter a username</p>
+        <p className="inputter">Enter a username</p>
         <input name="username" 
-        value={username}
+        value={formState.username}
         onChange={handleInputChange}
         type="text"
         placeholder="Username" 
         required />
 
-        <p>Enter an email</p>
+        <p className="inputter">Enter an email</p>
         <input name="email" 
-        value={email}
+        value={formState.email}
         onChange={handleInputChange}
         type="email"
         placeholder="Email" 
         required />
-        <p>Enter a password</p>
+        <p className="inputter">Enter a password</p>
         <input
           name="password"
-          value={password}
+          value={formState.password}
           onChange={handleInputChange}
           type="password"
           placeholder="Password"
