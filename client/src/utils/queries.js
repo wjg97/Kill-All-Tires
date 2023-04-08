2b727a9c6051914c86f681c3554910f9e2447d69
DylanCas <djcasabona12@gmail.com>
2023-04-07 20:00:53 -0600

    With Logan, implemented appointment form. Will likely need to alter logic to accept in such format

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,5 +1,33 @@
 import { gql } from '@apollo/client';
 
+export const QUERY_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+      password
+      vehicles {
+        _id
+        make
+        model
+        year
+      }
+      appointments {
+        _id
+        date
+        time
+        vehicle {
+          _id
+          make
+          model
+          year
+        }
+      }
+    }
+  }
+`;
+
 export const QUERY_USER = gql`
   query user($username: String!) {
     user(username: $username) {
@@ -26,6 +54,18 @@ export const QUERY_VEHICLE = gql`
 export const QUERY_SINGLE_APPOINTMENT = gql`
   query getSingleAppointment($appointmentId: ID!) {
     appointment(appointmentId: $appointmentId) {
+      _id
+      date
+      time
+      vehicle
+      user
+    }
+  }
+`;
+
+export const QUERY_APPOINTMENTS = gql`
+  query getAppointments {
+    appointments {
       _id
       date
       time
