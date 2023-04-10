9cf5d05ad1ac4aa8f597cf121ecf1efbe19ed51b
Persistent <agrlarbig@gmail.com>
2023-04-10 12:03:06 -0600

    added logic to hide signup and login pages after a user logs in

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -8,8 +8,20 @@ import SignUp from '../pages/signupForm/signup';
 import Appt from "./appointmentForm/Appt";
 import Garage from "../pages/garage/garage";
 
+
 export default function PortfolioContainer() {
   const [currentPage, setCurrentPage] = useState('Home');
+  const [ isLoggedIn, setIsLoggedIn ] = useState(false);
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+    setCurrentPage('Home'); // This will redirect the user to the home page after they log in
+  }
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    setCurrentPage('Home'); // This will redirect the user to the home page after they log out
+  }
 
   // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
   const renderPage = () => {
@@ -19,26 +31,28 @@ export default function PortfolioContainer() {
     if (currentPage === 'Contact') {
       return <Contact />;
     }
-    if (currentPage === 'Login') {
-      return <Login />;
+    if (currentPage === 'Login' && !isLoggedIn) {
+      return <Login handleLogin={handleLogin}/>;
     }
-    if (currentPage === 'Signup') {
+    if (currentPage === 'Signup' && !isLoggedIn) {
       return <SignUp />;
     }
-    if (currentPage === 'Appt') {
+    if (currentPage === 'Appt' && isLoggedIn) {
       return <Appt />;
     }
-    if (currentPage === 'Garage') {
+    if (currentPage === 'Garage' && isLoggedIn) {
       return <Garage />;
     }
   };
+
+  // Here we are setting the state for the currentPage. We pass this value and a function to update it to the Navbar component.
 
   const handlePageChange = (page) => setCurrentPage(page);
 
   return (
     <div>
       {/* We are passing the currentPage from state and the function to update it */}
-      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} isLoggedIn={isLoggedIn} handleLogout={handleLogout} />
       {/* Here we are calling the renderPage method which will return a component  */}
       {renderPage()}
     </div>
